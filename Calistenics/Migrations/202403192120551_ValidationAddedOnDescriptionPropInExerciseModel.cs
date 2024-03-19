90fce6fe7f003bc3a3d4c56428301f723717fb8d
Boban Milosevik <bobavtokom888@gmail.com>
2024-03-19 22:24:33 +0100

    validation added on model prop

---
+++ Calistenics/Migrations/202403192120551_ValidationAddedOnDescriptionPropInExerciseModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Calistenics.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ValidationAddedOnDescriptionPropInExerciseModel : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Excercises", "Description", c => c.String(maxLength: 200));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Excercises", "Description", c => c.String());
+        }
+    }
+}
--- Calistenics/Models/Excercise.cs
+++ Calistenics/Models/Excercise.cs
@@ -9,6 +9,7 @@ namespace Calistenics.Models {
         public int Id { get; set; }
         [Required]
         public string Name { get; set;}
+        [StringLength(200, ErrorMessage = "please insert minimum 6  up to 200 characters", MinimumLength = 6)]  
         public string Description { get; set;}
         public string Level { get; set;}
         [Url(ErrorMessage = "Invalid URL format. Please include 'http://' or 'https://'")]
